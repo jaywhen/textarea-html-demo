91037fb2d422b80c2f5e8140f2098396bbf25624
jaywhen <jaywhenx@gmail.com>
2023-08-12 22:39:27 +0800

    add a more nice way to handle value

--- test.js
+++ test.js
@@ -1,6 +1,38 @@
-const str = '2024 {{Preview}}: Bidenomics {{Versus}} the Trump {{Freak}} Show';
-const regex = /({{.*?}})/g;
+const contentPattern = /{{[\w]*$/g;
+const tipPattern = /({{.*?}})/g;
 
-const result = str.split(regex);
+function getContent(str) {
+  return str.split(tipPattern).map(item => {
+    if(item.match(tipPattern)) {
+      return {
+        type: 'val',
+        value: item
+      }
+    }
 
-console.log(result);
+    return {
+      type: 'common',
+      value: item
+    }
+  })
+}
+
+function checkAndGetString(string) {
+  console.log('origin string:  ', `|${string}|`)
+  const match = string.match(contentPattern);
+  if(match) {
+    const content = getContent(string.split(contentPattern)[0]);
+    content.push({
+      type: 'tip',
+      value: match[0]
+    })
+
+    return content;
+  }
+
+  return getContent(string);
+}
+
+console.log(checkAndGetString("Hello {{exp}} {{asd"));
+console.log(checkAndGetString("This is not the end {{asdd {{ "));
+console.log(checkAndGetString("World {{example"));
